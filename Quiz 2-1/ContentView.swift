50dc415df879627a1ad94d05b150f43f52e46827
Ian Pulsifer <s016572@students.lmsd.org>
2022-12-07 12:06:43 -0500

    Initial Commit

---
+++ Quiz 2-1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quiz 2-1
+//
+//  Created by Ian Pulsifer (student LM) on 12/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quiz 2-1/Quiz_2_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_2_1App.swift
+//  Quiz 2-1
+//
+//  Created by Ian Pulsifer (student LM) on 12/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_2_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
