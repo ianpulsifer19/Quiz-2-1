b183f7bbc16d3f4507f1dc1ad74ecf8159573520
Ian Pulsifer <s016572@students.lmsd.org>
2022-12-07 12:47:38 -0500

    Quiz 2-1

--- Quiz 2-1/ContentView.swift
+++ Quiz 2-1/ContentView.swift
@@ -8,12 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var picture: String = "forest"
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        ZStack{
+            Image(picture).resizable().aspectRatio(contentMode: .fill).edgesIgnoringSafeArea(.all)
+            VStack{
+                Spacer()
+                Button{
+                    if picture == "forest"{
+                        picture = "snowflake"
+                    }
+                    else{
+                        picture = "forest"
+                    }
+                }label:{
+                    Text("Let it snow").padding().background(Color.white).cornerRadius(20).foregroundColor(Color.purple).padding().font(.largeTitle)
+                }
+            }
+        }
 }
-
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
